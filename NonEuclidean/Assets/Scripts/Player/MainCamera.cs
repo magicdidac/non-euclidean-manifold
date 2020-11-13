82ca00fa5a9a4a5686cf4ea52bd7ff4e10e13f1c
Dídac <didacpadilla20@gmail.com>
2020-11-13 12:33:04 +0100

    Project Added

---
+++ NonEuclidean/Assets/Scripts/Player/MainCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+
+    Portal[] portals;
+    MinePortal[] minePortals;
+
+    void Awake()
+    {
+        portals = FindObjectsOfType<Portal>();
+        minePortals = FindObjectsOfType<MinePortal>();
+    }
+
+    void OnPreCull()
+    {
+        for (int i = 0; i < portals.Length; i++)
+        {
+            portals[i].Render();
+        }
+
+        for (int i = 0; i < minePortals.Length; i++)
+        {
+            minePortals[i].Render();
+        }
+
+        for (int i = 0; i < portals.Length; i++)
+        {
+            portals[i].PostPortalRender();
+        }
+
+    }
+
+}
